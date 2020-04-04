b43e2cdb0f8ff7c0669177761f5470c94e5ae30d
Yul3n <39010883+Yul3n@users.noreply.github.com>
2020-04-04 09:34:42 +0200

    Continued the lexer.

--- src/lexer.ml
+++ src/lexer.ml
@@ -11,18 +11,21 @@ type token
   | MOD
   | ARROW
   | DOT
-  | COMMA
-  | LBRACKET
-  | RBRACKET
-  | CONS
-  | APPEND
-  | WILDCARD
-  | IDENT  of string
-  | FLOAT  of float
-  | INT    of int
-  | STRING of string 
+  | EQUAL
+  | IDENT of string
+  | FLOAT of float
+  | INT   of int 
 
 let rec lexer input pos =
-  match String.length input with
-    len when len = pos -> []
-  | _ -> 
+  try
+    match String.get input pos with
+      '+'  -> PLUS      :: lexer input (pos + 1)
+    | '='  -> EQUAL     :: lexer input (pos + 1)
+    | '.'  -> DOT       :: lexer input (pos + 1)
+    | '\\' -> BACKSLASH :: lexer input (pos + 1)
+    | '('  -> LPARENT   :: lexer input (pos + 1)
+    | ')'  -> RPARENT   :: lexer input (pos + 1)
+    | ']'  -> LBRACKET  :: lexer input (pos + 1)
+    | '['  -> RBRACKET  :: lexer input (pos + 1)
+    | ','  -> COMMA     :: lexer input (pos + 1)
+  with _ -> [] 
