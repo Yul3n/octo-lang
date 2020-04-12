4647bf013e28edcee3d66998cb6f07cdd6c1022b
Yul3n <aghilas.boussaa@laposte.net>
2020-04-12 10:50:56 +0200

    Added a bit of syntax highlighting.

---
+++ tools/octo-mode.el
@@ -0,0 +1,19 @@
+;;; octo-mode.el --- A simple major mode providing syntax highlighting for octo-lang.
+;;; -*- coding: utf-8; lexical-binding: t; -*-
+;;; Commentary:
+
+;;; Code:
+
+(defconst octo-highlights
+  '(("where" . font-lock-keyword-face)
+    ("--.*\n" . font-lock-comment-face)
+    ("\n\\([a-zA-Z]*\\)\\([a-zA-Z _]*\\)=" . (2 font-lock-variable-name-face))
+    ("\n\\([a-zA-Z]*\\).*=" . (1 font-lock-function-name-face))))
+
+
+(define-derived-mode octo-mode fundamental-mode "octo"
+  "major mode for editing octo language code."
+  (setq font-lock-defaults '(octo-highlights)))
+
+(provide 'octo-mode)
+;;; octo-mode ends here
