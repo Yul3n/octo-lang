67d0a70e664aa6f8fca071975b850d771cc1d672
Yul3n <aghilas.boussaa@laposte.net>
2020-04-12 15:37:40 +0200

    Added automatic indentation.

--- tools/octo-mode.el
+++ tools/octo-mode.el
@@ -10,6 +10,39 @@
     ("\n\\([a-zA-Z]*\\)\\([a-zA-Z _]*\\)=" . (2 font-lock-variable-name-face))
     ("\n\\([a-zA-Z]*\\).*=" . (1 font-lock-function-name-face))))
 
+(defun indent-line ()
+  "Indent current line as WPDL code."
+  (interactive)
+  (let ((cur-indent 0))
+    (beginning-of-line)
+    (if (looking-at "\\([a-zA-Z]*\\).*=")
+        (indent-line-to 0)
+      (progn
+        (forward-line -1)
+        (beginning-of-line)
+        (if (looking-at "\\([a-zA-Z]*\\).*=")
+            (progn
+              (forward-line 1)
+              (indent-line-to 2)
+              )
+          (let (b e)
+            (setq b (point))
+            (skip-chars-forward " ")
+            (setq e (point))
+            (if (looking-at "[ ]*where\n")
+                (progn
+                   (forward-line 1)
+                   (indent-line-to (+ (- e b) 2)))
+              (progn
+                (forward-line 1)
+                (indent-line-to (- e b) ))
+              )
+            )
+          )
+        )
+      )
+    )
+  )
 (define-derived-mode octo-mode fundamental-mode "octo"
   "major mode for editing octo language code."
   (setq comment-add "-- "
@@ -18,7 +51,10 @@
         comment-start "-- "
         comment-end ""
         comment-auto-fill-only-comments t
-        font-lock-defaults '(octo-highlights)))
+        font-lock-defaults '(octo-highlights)
+        )
+  (set (make-local-variable 'indent-line-function) 'indent-line)
+  )
 
 (provide 'octo-mode)
 ;;; octo-mode ends here
